627311c75a641e125880469f44474e71aa80cdb0
PIeter <pieter_ophalvens@hotmail.com>
2024-01-08 15:37:58 +0100

    ex2 complete

--- ex2.php
+++ ex2.php
@@ -1,20 +1,57 @@
 <?php
 
 declare(strict_types=1);
+class Beverage
+{
+  public string $color;
+  public float $price;
+  public string $temperature;
+  public function __construct(string $color, float $price, string $temperature = 'cold')
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = $temperature;
 
-/* EXERCISE 2
+  }
+  //Make a getInfo function which returns "This beverage is <temperature> and <color>."
+  public function getInfo()
+  {
+    echo "This beverage is " . $this->temperature . " and " . $this->color;
+  }
+}
 
-TODO: Make class beer that extends from Beverage.
-TODO: Create the properties name (string) and alcoholPercentage (float).
-TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholpercentage.
+// EXERCISE 2
 
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getAlcoholPercentage function which returns the alocoholPercentage.
-TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
-TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
-TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+//  Make class beer that extends from Beverage.
+class Beer extends Beverage
+{
+  //  Create the properties name (string) and alcoholPercentage (float).
+  public string $properties;
+  public float $alcoholPercentage;
 
-Make sure that each print is on a different line.
-Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
-USE TYPEHINTING EVERYWHERE!
-*/
+  //  Foresee a construct that's allows us to use all the properties from beverage and 
+  //that sets the values for name and alcoholpercentage.
+  public function __construct(string $color, float $price, string $temperature = 'cold', string $properties, float $alcoholPercentage)
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = $temperature;
+    $this->properties = $properties;
+    $this->alcoholPercentage = $alcoholPercentage;
+  }
+  //  Make a getAlcoholPercentage function which returns the alocoholPercentage.
+  public function getAlcoholPercentage()
+  {
+    echo $this->alcoholPercentage;
+  }
+}
+//  Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+//  Also the name equal to Duvel and the alcohol percentage to 8,5%
+$duvel = new Beer('blond', 3.5, 'cold', 'Duvel', 8.5);
+//  Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+echo $duvel->getAlcoholPercentage() . "<br>";
+echo $duvel->alcoholPercentage . "<br>";
+echo $duvel->color . "<br>";
+
+// Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
+// USE TYPEHINTING EVERYWHERE!
